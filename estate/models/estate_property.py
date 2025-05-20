30618652a532fcb84e281dd32d9f56fef9ab5517
hariom-saw <hariomsaw@gmail.com>
2025-05-20 09:53:20 +0530

    Chap 8

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,51 @@
-from odoo import models, fields
-from odoo import models, fields
+from odoo import _, api, fields, models
+
 
 class EstateProperty(models.Model):
-    _name = 'estate.property'
-    _description = 'Real Estate Property'
+    _name = "estate.property"
+    _description = "Real Estate Property"
 
-    name = fields.Char(string='Title', required=True)
+    name = fields.Char(string="Title", required=True)
     description = fields.Text()
     # Many2many Fields
     tag_ids = fields.Many2many("estate.property.tag", string="Property Tags")
     # One2many Fields
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
     # Many2one Fields
+
+    total_area = fields.Float(compute="_compute_total_area", string="Total Area (sqm)")
+    best_price = fields.Float(compute="_compute_best_price", string="Best Offer")
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = sum([record.living_area, record.garden_area])
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped("price"))
+            else:
+                record.best_price = 0.0
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = "north"
+            return {
+                "warning": {
+                    "title": _("Warning"),
+                    "message": (
+                        "This option will enable Garden Area (default: 10) & Orientation (default: North)"
+                    ),
+                }
+            }
+        else:
+            self.garden_area = None
+            self.garden_orientation = None
+
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     buyer_id = fields.Many2one("res.partner", string="Buyer")
     salesman_id = fields.Many2one("res.users", string="Salesman")
@@ -28,17 +62,21 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection=[
-            ('north', 'North'),
-            ('south', 'South'),
-            ('east', 'East'),
-            ('west', 'West')
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
         ],
-        string='Garden Orientation'
+        string="Garden Orientation",
     )
-    state = fields.Selection([
-    ('new', 'New'),
-    ('offer_received', 'Offer Received'),
-    ('offer_accepted', 'Offer Accepted'),
-    ('sold', 'Sold'),
-    ('cancelled', 'Cancelled')
-], default='new', string='Status')
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("cancelled", "Cancelled"),
+        ],
+        default="new",
+        string="Status",
+    )
